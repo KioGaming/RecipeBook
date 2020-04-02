610bcdf08510a658c178952be301c4580812915e
Vova_Olexienko <olexienko.vova.k@gmail.com>
2020-04-02 18:09:17 +0300

    init

--- src/login/Configs.java
+++ src/login/Configs.java
@@ -2,9 +2,15 @@ package login;
 
 public class Configs {
 
-    protected String dbHost = "localhost";
+    /*protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
     protected String dbPass = "Hv_UoPiol1Gd";
-    protected String dbName = "recipebook";//recipe_book
+    protected String dbName = "recipebook";//recipe_book*/
+
+    protected String dbHost = "sql104.byethost.com";
+    protected String dbPort = "3306";
+    protected String dbUser = "b11_25449384";
+    protected String dbPass = "Vova_2003";
+    protected String dbName = "b11_25449384_recipebook";
 }
